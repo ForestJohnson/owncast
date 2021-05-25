8b1bb2866b1174bd0b4f69d862711f13a380c908
forest <forest.n.johnson@gmail.com>
2021-05-25 14:02:21 -0500

    chat client provides its initial username when it connects
    this keeps the client and server view of the chat clients
    more consistent so consumers of the API
    (for example, /api/admin/clients) will get more accurate data

--- core/chat/client.go
+++ core/chat/client.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"strings"
 	"time"
 
 	log "github.com/sirupsen/logrus"
@@ -48,10 +49,14 @@ func NewClient(ws *websocket.Conn) *Client {
 		log.Panicln("ws cannot be nil")
 	}
 
+	var initialUsername string
 	var ignoreClient = false
 	for _, extraData := range ws.Config().Protocol {
 		if extraData == "IGNORE_CLIENT" {
 			ignoreClient = true
+		}
+		if strings.HasPrefix(extraData, "INITIAL_USERNAME") {
+			initialUsername = strings.TrimPrefix(extraData, "INITIAL_USERNAME")
 		}
 	}
 
@@ -68,7 +73,7 @@ func NewClient(ws *websocket.Conn) *Client {
 
 	rateLimiter := rate.NewLimiter(0.6, 5)
 
-	return &Client{time.Now(), 0, userAgent, ipAddress, nil, clientID, nil, ignoreClient, socketID, ws, ch, pingch, usernameChangeChannel, userJoinedChannel, doneCh, rateLimiter}
+	return &Client{time.Now(), 0, userAgent, ipAddress, &initialUsername, clientID, nil, ignoreClient, socketID, ws, ch, pingch, usernameChangeChannel, userJoinedChannel, doneCh, rateLimiter}
 }
 
 func (c *Client) write(msg models.ChatEvent) {
